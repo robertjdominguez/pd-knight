6ccd5741308462f1c0dfb3d4fec3aa69c534d674
robertjdominguez <rtjdominguez@gmail.com>
2021-03-10 10:49:39 -0600

    UX improvements for reg process

--- pages/confirmation/[slug].js
+++ pages/confirmation/[slug].js
@@ -4,26 +4,36 @@ import { useSession } from "next-auth/client";
 import { useState } from "react";
 import Router from "next/router";
 import graphcms from "../../components/utilities/graphCMS";
-import { PageWrapper, NavCTA } from "../../components/layout/Lib";
+import {
+  PageWrapper,
+  NavCTA,
+  SessionDeets,
+  SessionItem,
+} from "../../components/layout/Lib";
 
 const Confirmation = ({ pdSession }) => {
   const [session, loading] = useSession();
-  session && console.log(session.user);
+  session && console.log(session && session.user);
 
   //   State for the registration progress, init with false
-  const [isRegistering, setIsRegistering] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(`pre`);
 
   //   Checking for status of registration from api route
   async function register() {
+    // Set the loader...
+    setIsRegistering(`loading`);
     //   async call to api route
     const registration = await axios.post("/api/webhooks/addInvite", {
       id: pdSession.teamsLink,
       emailAddress: {
-        email: session.user.email,
-        name: session.user.name,
+        email: session && session.user.email,
+        name: session && session.user.name,
       },
     });
-    console.log(registration.data.message);
+    // Bring up the confirmation message
+    registration.status == 200
+      ? setIsRegistering(`complete`)
+      : setIsRegistering(`error`);
   }
 
   //   If not authenticated, push them back to the index
@@ -33,17 +43,97 @@ const Confirmation = ({ pdSession }) => {
 
   return (
     <>
-      <PageWrapper>
-        <h2>Confirming for {pdSession.title}</h2>
-        <NavCTA
-          style={{
-            fontSize: `12px`,
-            marginTop: `20px`,
-            placeSelf: `start start`,
-          }}
-          onClick={register}>
-          CONFIRM REGISTRATION
-        </NavCTA>
+      <PageWrapper style={{ placeItems: `center center` }}>
+        <SessionDeets style={{ background: `var(--eggshell)` }}>
+          <div
+            style={{
+              display: isRegistering == `pre` ? `grid` : `none`,
+            }}>
+            <h3 style={{ marginTop: `0`, marginBottom: `3vh` }}>
+              Confirm your registration for {pdSession.title}
+            </h3>
+            <ul>
+              <SessionItem
+                icon={"/calendar2.svg"}
+                style={{ fontSize: `18px`, color: `var(--black)` }}>
+                <Moment format="DD MMMM HH:MM a" date={pdSession.date} />
+              </SessionItem>
+              <SessionItem
+                icon={"/watch.svg"}
+                style={{ fontSize: `14px`, color: `var(--black)` }}>
+                {pdSession.hours} hours
+              </SessionItem>
+              <SessionItem
+                icon={"/user2.svg"}
+                style={{ fontSize: `14px`, color: `var(--black)` }}>
+                {pdSession.leader.name.split(" ")[0]}
+              </SessionItem>
+            </ul>
+            <NavCTA
+              style={{
+                fontSize: `12px`,
+                marginTop: `3vh`,
+                placeSelf: `start start`,
+              }}
+              onClick={register}>
+              CONFIRM REGISTRATION
+            </NavCTA>
+          </div>
+          <div
+            style={{
+              placeItems: `center center`,
+              display: isRegistering == `loading` ? `grid` : `none`,
+            }}>
+            <img
+              src="/math.gif"
+              style={{ borderRadius: `13px` }}
+              width="100%"
+              height="auto"
+              alt="Math scene from The Hangover"
+            />
+            <h4>
+              Taking care of the difficult part,{" "}
+              {session && session.user.name.split(" ")[0]}...give us a minute.
+            </h4>
+          </div>
+          <div
+            style={{
+              placeItems: `center center`,
+              display: isRegistering == `complete` ? `grid` : `none`,
+            }}>
+            <img
+              src="/benwyatt.gif"
+              style={{ borderRadius: `13px` }}
+              width="200px"
+              height="auto"
+              alt="Math scene from The Hangover"
+            />
+            <h4 style={{ fontWeight: `600` }}>
+              Okay...you're good to go,{" "}
+              {session && session.user.name.split(" ")[0]}! Check your calendar
+              for an invite. <br />
+              <br /> See you on{" "}
+              <Moment format="dddd MMMM Do" date={pdSession.date} />!
+            </h4>
+          </div>
+          <div
+            style={{
+              placeItems: `center center`,
+              display: isRegistering == `error` ? `grid` : `none`,
+            }}>
+            <img
+              src="/dr.gif"
+              style={{ borderRadius: `13px` }}
+              width="100%"
+              height="auto"
+              alt="Math scene from The Hangover"
+            />
+            <h4 style={{ fontWeight: `600` }}>
+              This is...embarrassing. A mistake has been made. Rest assured,
+              we're on it.
+            </h4>
+          </div>
+        </SessionDeets>
       </PageWrapper>
     </>
   );
